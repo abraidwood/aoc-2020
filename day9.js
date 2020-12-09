bfb8d37915e3b4e44c1c88369f15549e7a40e936
Alistair Braidwood <alistair_braidwood@yahoo.co.uk>
2020-12-09 09:31:54 +0000

    day9: js performance

--- day9.js
+++ day9.js
@@ -2,6 +2,8 @@ const { run, readFile } = require('./utils');
 
 const values = readFile('data/day9.txt').map(v => parseInt(v, 10));
 
+let part1Result;
+
 function day6part1() {
     const preambleLength = 25;
     const preambleList = values.slice(0, preambleLength);
@@ -13,6 +15,7 @@ function day6part1() {
         if (!preambleList.some(
             item => item !== value && preambleSet.has(value - item)
         )) {
+            part1Result = value;
             return value;
         }
 
@@ -23,16 +26,15 @@ function day6part1() {
 }
 
 function day6part2() {
-    const result = day6part1();
     let start = 0;
     let end = 0;
     let sum = values[start];
 
     while (start < values.length && end < values.length) {
-        if (sum < result) {
+        if (sum < part1Result) {
             end += 1;
             sum += values[end];
-        } else if (sum > result) {
+        } else if (sum > part1Result) {
             sum -= values[start];
             start += 1;
         } else {
